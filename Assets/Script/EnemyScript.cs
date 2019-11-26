dd2cc9e2631c400e03b55864da97ee59249d0abb
Fazlul69 <sawravm299@gmail.com>
2019-11-26 10:09:07 +0600

    code

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+
+    public Transform[] wayPoints;
+    public Transform ExitPoint;
+    public int target;
+    public float navigationTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = 0;
+        navigationTime = 0;
+}
+
+// Update is called once per frame
+void Update()
+    {
+    if (wayPoints.Length > target) {
+            navigationTime = Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position,wayPoints[target].position, navigationTime);
+    }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "CheckPoint") {
+            Debug.Log("Trigger");
+            target += 1;
+        }
+        if (col.gameObject.tag == "ExitPoint")
+        {
+            Destroy(gameObject);
+        }
     }
 }
